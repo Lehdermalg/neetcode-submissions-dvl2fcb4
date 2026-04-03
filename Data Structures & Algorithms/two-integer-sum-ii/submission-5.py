da50a0ba0943b549c4b97db4eca80b5d1dd0ddb5
Lehdermalg <160425616+Lehdermalg@users.noreply.github.com>
2026-04-03 21:44:24 +0200

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        ln = len(numbers)
+        for li,n in enumerate(numbers):
+            diff = target - n
+            ri = ln-1
+            if diff in numbers:
+                # find the match's index
+                while ri > li and numbers[ri] != diff:
+                    ri -= 1
+                return [li+1, ri+1]
+        return [-1, -1]        
+
